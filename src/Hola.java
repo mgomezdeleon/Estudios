a1da0c2144d12494f54877f5e9825506247d2fcb
Marta <mgomezdeleon@ceinmark.net>
2025-05-07 10:23:08 +0200

    Modificada Rama3

--- src/Hola.java
+++ src/Hola.java
@@ -3,6 +3,7 @@ public class Hola {
 	public static void main(String[] args) {
 		
 		System.out.println("Rama37");
-		System.out,println("Martes");
+		System.out.println("Martes");
+		System.out.println("Rama3");
 	}
 }
